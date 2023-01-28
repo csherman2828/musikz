e8ceb95276797448a6a241c1b0556be18f68b88e
csherman2828 <csherman2828@gmail.com>
2023-01-28 01:03:06 -0500

    decide whether to run dev or prod music bot

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 import json
+from sys import argv
 
 import discord
 from discord.ext import commands
 
 import music
 
-bot = commands.Bot(command_prefix='|', description="Daj eno zgodlej")
+_config = None
+_token = ''
+with open('config.json') as fh:
+    _config = json.load(fh)
+
+if __name__ == "__main__" and len(argv) > 1 and argv[1] == 'dev':
+    _command_prefix = '?'
+    _token = _config['dev_token']
+else:
+    _command_prefix = '!'
+    _token = _config['token']
+
+bot = commands.Bot(command_prefix=_command_prefix, description="Daj eno zgodlej")
 
 @bot.event
 async def on_ready():
@@ -16,10 +29,7 @@ async def on_ready():
     bot.add_cog(music.Music(bot))
 
 def main():
-    with open('config.json') as fh:
-        bot.config = json.load(fh)
-
-    bot.run(bot.config['token'])
+    bot.run(_token)
 
 
 if __name__ == "__main__":
