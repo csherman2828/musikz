e5e7905436872003caedca29aab8a941a67193c3
csherman2828 <csherman2828@gmail.com>
2023-02-03 20:44:28 -0500

    Revert "decide whether to run dev or prod music bot"
    This reverts commit e8ceb95276797448a6a241c1b0556be18f68b88e.

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
 import json
-from sys import argv
 
 import discord
 from discord.ext import commands
 
 import music
 
-_config = None
-_token = ''
-with open('config.json') as fh:
-    _config = json.load(fh)
-
-if __name__ == "__main__" and len(argv) > 1 and argv[1] == 'dev':
-    _command_prefix = '?'
-    _token = _config['dev_token']
-else:
-    _command_prefix = '!'
-    _token = _config['token']
-
-bot = commands.Bot(command_prefix=_command_prefix, description="Daj eno zgodlej")
+bot = commands.Bot(command_prefix='|', description="Daj eno zgodlej")
 
 @bot.event
 async def on_ready():
@@ -29,7 +16,10 @@ async def on_ready():
     bot.add_cog(music.Music(bot))
 
 def main():
-    bot.run(_token)
+    with open('config.json') as fh:
+        bot.config = json.load(fh)
+
+    bot.run(bot.config['token'])
 
 
 if __name__ == "__main__":
